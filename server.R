a9fd2aa78b43987cca4499c44db2e50ab26d6779
Wendy <vwendy@gmail.com>
2020-12-21 18:10:44 -0500

    add functions to do data viz, model, impulse response analysis

--- server.R
+++ server.R
@@ -8,21 +8,228 @@
 #
 
 library(shiny)
-
 library(ggplot2)
+library(reshape2)
+library(pompom)
+
+set.seed(1234)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    dataset <- reactive({
-        get(input$dataset, "package:ggplot2")
+    
+    mySeries_raw <- reactive({
+        inFile <- input$i_file
+        if (is.null(inFile))
+            return(NULL)
+        mySeries <- read.csv(inFile$datapath, 
+                             header = T,
+                             strip.white=T,
+                             stringsAsFactors=F,
+                             fill=T)
     })
+    
+    usem <- reactive({
+        # how ot pass parameters into reactive, e.g., var.number
+        # and out of reactive, e.g., mdl versus model.fit
+        
+        mySeries <- mySeries_raw()
+        # mySeries <- read.csv("c:/users/Xiao Yang/Downloads/time-series.csv")
+        var.number <- ncol(mySeries) # number of variables
+        lag.order <- 1 # lag order of the model
+        model.fit <- uSEM(var.number, 
+                          mySeries,
+                          lag.order, 
+                          verbose = FALSE, 
+                          trim = TRUE)
+
+        
+    })
+    
+    output$datatab <- renderPrint({
+        mySeries <- mySeries_raw()
+        names(mySeries) <- paste("v", 1:ncol(mySeries), sep ="")
+        print(head(mySeries))
+    })
+    
     output$summmry <- renderPrint({
-        summary(dataset())
+        mySeries <- mySeries_raw()
+        print(summary(mySeries))
     })
-    output$plot <- renderPlot({
-        plot(dataset())
+    
+    output$dataviz <- renderPlot({
+        mySeries <- mySeries_raw()
+
+        # mySeries <- read.csv("c:/users/Xiao Yang/Downloads/shiny example data.csv")
+
+        col_no <- ncol(mySeries)
+        names(mySeries) <- paste("v",1:col_no)
+        mySeries$index <- 1:nrow(mySeries)
+
+        mySeries_long <- reshape(mySeries,
+                                 timevar = "index",
+                                 varying = list(1:col_no),
+                                 direction = "long")
+        names(mySeries_long) <- c("variable","value","index")
+        ggplot(mySeries_long, aes(x = index, y = value, color = factor(variable)))+
+            geom_line()+
+            facet_wrap(~variable, ncol = 1)+
+            theme_classic()
+
     }, res = 96)
     
+    output$modelnetwork <- renderPlot({
+        mySeries <- mySeries_raw()
+        # # mySeries <- read.csv("c:/users/Xiao Yang/Downloads/time-series.csv")
+        var.number <- ncol(mySeries) # number of variables
+        lag.order <- 1 # lag order of the model
+        
+        # model.fit <- uSEM(var.number, 
+        #                   mySeries,
+        #                   lag.order, 
+        #                   verbose = FALSE, 
+        #                   trim = TRUE)
+        # 
+        
+        model.fit <- usem()
+        mdl <- model_summary(model.fit,
+                             var.number,
+                             lag.order)
+
+        
+        
+        if(length(mdl)==1 ){
+            
+            # show something to let people know it didn't work 
+        }
+        else{
+            rule <- ifelse(mdl$cfi >= 0.95,1,0) + 
+                ifelse(mdl$tli >= 0.95,1,0) + 
+                ifelse(mdl$rmsea <= 0.08 ,1,0) + 
+                ifelse(mdl$srmr <= 0.08,1,0)
+        
+            if (rule >=3){
+                beta.matrix <- parse_beta(var.number = var.number, 
+                                          model.fit = model.fit, 
+                                          lag.order = lag.order, 
+                                          matrix = TRUE) # parse temporal relations in matrix format
+                
+                plot_network_graph(beta.matrix$est, 
+                                   var.number)
+            }
+            else{
+                # show something to let people know it didn't work 
+            }
+        }
+        
+    }, res = 96)
+    
+    
+    
+    output$impulse <- renderPlot({
+        mySeries <- mySeries_raw()
+        # mySeries <- read.csv("c:/users/Xiao Yang/Downloads/time-series.csv")
+        var.number <- ncol(mySeries) # number of variables
+        lag.order <- 1 # lag order of the model
+        threshold <- 0.01
+        replication <- 200
+        steps <- 100
+        
+        # model.fit <- uSEM(var.number, 
+        #                   mySeries,
+        #                   lag.order, 
+        #                   verbose = FALSE, 
+        #                   trim = TRUE)
+        # 
+ 
+ 
+        model.fit <- usem()
+        mdl <- model_summary(model.fit,
+                             var.number,
+                             lag.order)
+        
+        
+        if(length(mdl)==1 ){
+            
+            # show something to let people know it didn't work 
+        }
+        else{
+            rule <- ifelse(mdl$cfi >= 0.95,1,0) + 
+                ifelse(mdl$tli >= 0.95,1,0) + 
+                ifelse(mdl$rmsea <= 0.08 ,1,0) + 
+                ifelse(mdl$srmr <= 0.08,1,0)
+            
+            if (rule >=3){
+                beta.matrix <- parse_beta(var.number = var.number, 
+                                          model.fit = model.fit, 
+                                          lag.order = lag.order, 
+                                          matrix = TRUE) # parse temporal relations in matrix format
+                
+                plot_network_graph(beta.matrix$est, 
+                                   var.number)
+                
+                # point.estimate.iRAM <- iRAM(model.fit, 
+                #                             beta = NULL, 
+                #                             var.number = var.number, 
+                #                             lag.order = lag.order, 
+                #                             threshold = threshold,
+                #                             boot = FALSE, 
+                #                             replication = replication,
+                #                             steps= steps)
+                # 
+                # # point.estimate.iRAM$recovery.time 
+                # 
+                # plot_time_profile(point.estimate.iRAM$time.series.data, 
+                #                   var.number = 3,
+                #                   threshold = threshold, 
+                #                   xupper = 50)
+                
+                bootstrap.iRAM <- iRAM(model.fit, 
+                                       beta = NULL, 
+                                       var.number = var.number, 
+                                       lag.order = lag.order,
+                                       threshold = threshold,
+                                       boot = TRUE, 
+                                       replication = replication,
+                                       steps= steps)
+                
+                plot_time_profile(bootstrap.iRAM$time.profile.data, 
+                                                         var.number = var.number,
+                                                         threshold = threshold, 
+                                                         xupper = 25)
+                
+                
+            }
+            else{
+                # show something to let people know it didn't work 
+            }
+        }
+        
+    }, res = 96)
+    
+    # mySeries_dataviz <- eventReactive(input$dataVizButton, {
+    #     #dependency on 'data viz' button being pressed
+    #     #input$dataVizButton
+    #     
+    #     if (nrow(mySeries_raw())==0) 
+    #         return()
+    #     
+    #     #use existing reactive structures
+    #     mySeries <- mySeries_raw()
+    #    
+    #     col_no <- ncol(mySeries)
+    #     names(mySeries) <- paste("v",1:col_no)
+    #     mySeries$index <- 1:nrow(mySeries)
+    # 
+    #     mySeries_long <- reshape(mySeries,
+    #                              timevar = "index",
+    #                              varying = list(1:col_no),
+    #                              direction = "long")
+    #     names(mySeries_long) <- c("variable","value","index")
+    #     ggplot(mySeries_long, aes(x = index, y = value, color = factor(variable)))+
+    #         geom_line()+
+    #         facet_wrap(~variable, ncol = 1)
+    #     print("hello")
+    # })
     
     
 
--- ui.R
+++ ui.R
@@ -9,33 +9,31 @@
 
 library(shiny)
 
-datasets <- data(package = "ggplot2")$results[c(2,8), "Item"]
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Model the dynamics in multivariate time-series as networks"),
-    # 
-    # # Sidebar with a slider input for number of bins
-    # sidebarLayout(
-    #     sidebarPanel(
-    #         sliderInput("bins",
-    #                     "Number of bins:",
-    #                     min = 1,
-    #                     max = 50,
-    #                     value = 30)
-    #     ),
-    # 
-    #     # Show a plot of the generated distribution
-    #     mainPanel(
-    #         plotOutput("distPlot")
-    #     )
-    # )
+    titlePanel("Model Multivariate Time-Series as Networks"),
     
-    selectInput("dataset", "Dataset", choices = datasets),
-    verbatimTextOutput("summary"),
-    plotOutput("plot")
+    # input file
+    sidebarPanel(
+        fileInput("i_file", "Upload your CSV file",
+                  accept = c(
+                      "text/csv",
+                      "text/comma-separated-values,text/plain",
+                      ".csv")
+        ),
+        # actionButton(inputId="dataVizButton", "Data visualization"),
+        # actionButton(inputId="modelButton", "Model time-series"),
+        # actionButton(inputId="impulseButton", "Impulse response analysis"),
+    ),
+    
+    verbatimTextOutput("datatab"),
+    # verbatimTextOutput("summary"),
+    plotOutput("dataviz"), 
+    plotOutput("modelnetwork"),
+    plotOutput("impulse")
     
     
     
